67d729d3a53d73fd43910498cc9e435cd81c2be2
skd9712 <skd97122@gmail.com>
2024-07-18 15:36:52 +0900

    오디오, 비디오리스트 처리

--- src/main/resources/static/js/exam/getfile.js
+++ src/main/resources/static/js/exam/getfile.js
@@ -32,6 +32,7 @@ window.onload = function () {
             console.log('new', newpath);
         }
     }
+    //img 태그불러오기
     let imgEle = document.getElementsByTagName('img');
     for (let i = 0; i < imgEle.length; i++) {
         let target = imgEle[i];
@@ -49,22 +50,6 @@ window.onload = function () {
         }
     }
 
-    // let sourceEle = document.getElementsByTagName('source');
-    // for (let i = 0; i < sourceEle.length; i++) {
-    //     let target = sourceEle[i];
-    //     console.log(sourceEle.src);
-    //     let path = decodeURIComponent(target.src);
-    //     console.log(path);
-    //     let arr = path.split("http://localhost:8080");
-    //     if (arr.length > 1) {
-    //         console.log(arr[0]);
-    //         console.log(arr[1]);
-    //         arr[0] = "https://kdt-java-5-2.s3.ap-northeast-2.amazonaws.com";
-    //         target.src = arr[0] + arr[1];
-    //         let newpath = decodeURIComponent(target.src);
-    //         console.log('new', newpath);
-    //     }
-    // }
     // 모든 <source> 요소를 선택
     let sourceElements = document.getElementsByTagName('source');
 
@@ -97,14 +82,41 @@ window.onload = function () {
     }
 
 
-    // let imgtarget = document.getElementsByTagName('img')[0];
-        // console.log(imgtarget.src);
-        // //한글경로를 decode
-        // let imgpath = decodeURIComponent(imgtarget.src);
-        // console.log(imgpath);
-        // let imgarr = path.split("/img");
-        // console.log(imgarr);
+    // 오디오 중지 불가, 다운 불가능 기능
+    let audioList = document.querySelectorAll('audio');
+    audioList.forEach((item, index) => {
+        item.setAttribute('controlslist', 'nodownload noplaybackrate'); // controlslist 속성 설정
 
-    }
+        item.addEventListener('play', (ev) => {
+            ev.target.currentTime = 0; // 재생 시간 초기화
+            ev.target.style.pointerEvents = 'none'; // pointer events 비활성화
+            ev.target.muted = false; // 음소거 해제
+        });
+    });
 
-// audiolist, videolist 메서드 여기서 활용
+    let videoList = document.querySelectorAll('video');
+
+    videoList.forEach((video) => {
+        // <video> 요소에 controlslist 속성을 설정합니다.
+        video.setAttribute('controlslist', 'nodownload noplaybackrate');
+
+        // Picture-in-Picture 기능을 비활성화합니다.
+        video.disablePictureInPicture = true;
+
+        // 비디오가 일시 정지되지 않도록 pause 이벤트를 감지하고 다시 재생합니다.
+        video.addEventListener('pause', (event) => {
+            video.play(); // 일시 정지된 경우 다시 재생
+        });
+
+        // // 비디오 재생을 시작할 때 다른 컨트롤을 비활성화할 수 있습니다.
+        // video.addEventListener('play', (event) => {
+        //     video.style.pointerEvents = 'none'; // 재생 중에 비디오 컨트롤 비활성화
+        // });
+    });
+
+    // let videoList = document.querySelectorAll('source');
+    // videoList.forEach((item,index)=>{
+    //     item.setAttribute('controlslist','nodownload noplaybackrate');
+    //     item.disablePictureInPicture = true;
+    // })
+}
