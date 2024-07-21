6ae143a9ee1158e0495f80c130092a694d57e694
tongueEye <109783402+tongueEye@users.noreply.github.com>
2024-07-21 22:59:26 +0900

    [feat] PDF 다운로드 구현 중  - A4 용지 크기에 맞게 html을 변환시켜 pdf 문서로 다운로드는 되지만, 현재 뷰포트 영역에 대해서만 출력됨

--- src/main/java/com/tive/service/ReportServiceImpl.java
+++ src/main/java/com/tive/service/ReportServiceImpl.java
@@ -8,6 +8,7 @@ import com.tive.repository.report.ReportRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 
 import java.util.HashMap;
--- src/main/resources/static/js/report/pdf.js
+++ src/main/resources/static/js/report/pdf.js
@@ -1,20 +1,45 @@
+
+function pdfPrint() {
+    var printArea = document.getElementById('print_area');
+    var options = {
+        scale: 2, // 스케일을 높여 해상도를 개선
+        useCORS: true, // 이미지가 다른 서버에 있을 경우 CORS 사용
+    };
+
+    html2canvas(printArea, options).then(function(canvas) {
+        var imgData = canvas.toDataURL('image/png');
+        var imgWidth = 210; // A4 size width in mm
+        var pageHeight = imgWidth * 1.414; // A4 size height in mm
+        var imgHeight = canvas.height * imgWidth / canvas.width;
+        var heightLeft = imgHeight;
+
+        var doc = new jsPDF('p', 'mm');
+        var position = 0;
+
+        // Add the first page
+        doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+
+        // Add new pages if content spans more than one page
+        while (heightLeft >= 20) {
+            position = heightLeft - imgHeight;
+            doc.addPage();
+            doc.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+            heightLeft -= pageHeight;
+        }
+
+        // Save the PDF
+        doc.save('sample.pdf');
+    });
+}
+
 window.addEventListener('load', function () {
+
+
+
     let print_btn = document.getElementById('print_btn');
 
     print_btn.addEventListener('click', function () {
-        let printArea = document.getElementById('print_area');
-        let opt = {
-            margin:       0.5,
-            filename:     'output.pdf',
-            image:        { type: 'jpeg', quality: 1 },
-            html2canvas:  { scale: 1 },
-            jsPDF:        { unit: 'in', format: 'b5', orientation: 'portrait' }
-        };
-
-        // html2pdf 라이브러리를 사용하여 PDF로 변환
-        html2pdf()
-            .from(printArea)
-            .set(opt)
-            .save();
+        pdfPrint();
     });
 })
