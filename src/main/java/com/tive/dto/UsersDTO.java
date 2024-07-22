982a7cdbf36730f3552946424cde87e3bdbef276
mira <pkm9997@gmail.com>
2024-07-22 11:27:54 +0900

    [fix]회원가입시 이메일 null값 들어오는거 수정

--- src/main/java/com/tive/dto/UsersDTO.java
+++ src/main/java/com/tive/dto/UsersDTO.java
@@ -11,14 +11,13 @@ import lombok.*;
 public class UsersDTO {
     private Long uid;
 
-    @NotEmpty(message = "입력해")
+    @NotEmpty(message = "이름은 필수 입력사항입니다")
     private String name;
 
-    //@NotEmpty(message = "입력해")
     //@Pattern(regexp = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}", message = "올바른 이메일 형식으로 입력하세요.")
     private String email;
 
-    @NotEmpty(message = "입력해")
+    @NotEmpty(message = "비밀번호는 필수 입력사항입니다")
     private String pwd;
 
     private String schoolLevel;
